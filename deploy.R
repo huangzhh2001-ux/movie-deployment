8860aaca1c003c77d1570854c2e7b8391f468e1a
huangzhh2001-ux <huangzhh2001@gmail.com>
2026-06-07 13:14:26 +0800

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,5 +1,5 @@
 rsconnect::setAccountInfo(
-  name = "huangzhaohua25085502",   # 替换为你的正确账户名（无短横线）
+  name = "huangzhaohua25085502",
   token = "88BCDD4282BA762E65041D55C5990639",
   secret = "PR5mrINEDiAe+o9ofyL8UaFfPcqNC+Ad3j51Fr9x"
 )
